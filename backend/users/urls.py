d9cffa1350047b214d50a4651d4aa826d8a31f21
Ishihara19 <stevenchicue@hotmail.com>
2025-02-11 11:11:31 -0500

    Correcion en la importacion en users/urls.py

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import CustomUserCreateView, CustomUserListView,UserRegisterAPIView
-from .authentication import  LoginView,RecuperarContraseñaView
+from .authentication import  LoginView,RecoverPasswordView
 urlpatterns = [
     path('users', CustomUserListView.as_view(), name='customuser-list'),  # Listar usuarios
     path('pre-register', CustomUserCreateView.as_view(), name='customuser-pre-register'),  # Pre-registro de usuarios
     path("register/<str:document>", UserRegisterAPIView.as_view(), name='customuser-register'),
     path('login', LoginView.as_view(), name='login'), # Login
-    path('recover-password', RecuperarContraseñaView.as_view(), name='recover-password'),
+    path('recover-password', RecoverPasswordView.as_view(), name='recover-password'),
 ]
