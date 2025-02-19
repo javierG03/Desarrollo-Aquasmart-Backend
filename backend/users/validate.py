61c397656b344cb98e972475ab2c05bb4c5cc422
Ishihara19 <stevenchicue@hotmail.com>
2025-02-19 12:07:03 -0500

    En users/validate.py se simplifico la funcion dado que en algunos momento daba error

--- backend/users/validate.py
+++ backend/users/validate.py
@@ -3,26 +3,11 @@ from rest_framework.exceptions import NotFound
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.exceptions import ValidationError
 def validate_user(document):
-    """
-    Valida la existencia de un usuario a partir de su documento de identidad.
-
-    Esta función busca un usuario en la base de datos utilizando el documento de identidad proporcionado.
-    Si el usuario existe, lo devuelve; de lo contrario, lanza una excepción indicando que no se encontró.
-
-    Parámetros:
-    - `document` (str): Número de documento de identidad del usuario.
-
-    Retorno:
-    - `CustomUser`: Instancia del usuario si existe en la base de datos.
-
-    Excepciones:
-    - `NotFound`: Si no se encuentra un usuario con el documento proporcionado.
-    """
-    try:
-        user = CustomUser.objects.get(document=document)
-        return user
-    except CustomUser.DoesNotExist:
-        raise NotFound({"detail": "Usuario no encontrado con este documento."})
+        """
+        Verifica si el usuario con el documento proporcionado existe.
+        """
+        
+        return CustomUser.objects.filter(document=document).first()
 def validate_otp(user, is_validated=False, otp=None):
     """
     Valida si existe un OTP según el estado de validación.
@@ -34,7 +19,7 @@ def validate_otp(user, is_validated=False, otp=None):
     Retorna el objeto OTP si existe, de lo contrario lanza un error.
     """
     try:
-        filters = {"user": user, "is_validated": is_validated}
+        filters = {"user": user, "is_validated": is_validated,}
         if otp is not None:
             filters["otp"] = otp  # Filtrar por OTP si se proporciona
         
@@ -45,4 +30,4 @@ def validate_otp(user, is_validated=False, otp=None):
             "No hay un OTP validado para este usuario."
             if is_validated else "OTP inválido o ya ha sido utilizado."
         )
-        raise ValidationError({"detail": message})
+        raise ValidationError({"detail": message})   
