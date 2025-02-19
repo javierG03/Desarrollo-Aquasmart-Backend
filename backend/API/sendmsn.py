867b1e1ec12e50b067131dce49919ffd93f89274
Ishihara19 <stevenchicue@hotmail.com>
2025-02-19 11:48:26 -0500

    Se saco la logica de envio de mensajes a un archivo aparte en API/sendmsn.py

---
+++ backend/API/sendmsn.py
@@ -0,0 +1,11 @@
+from django.core.mail import send_mail
+
+def send_email_recover(email, otp_generado):
+        """
+        Envía un correo con el OTP de recuperación de contraseña.
+        """
+        asunto = "Recuperación de Contraseña"
+        mensaje = f"Su OTP de recuperación es: {otp_generado}. Úselo para restablecer su contraseña."
+        remitente = "noreply@example.com"
+        send_mail(asunto, mensaje, remitente, [email])
+        
