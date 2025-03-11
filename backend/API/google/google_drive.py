e24cb798277b832c61e9c83ffc686f7d95612b84
Ishihara19 <stevenchicue@hotmail.com>
2025-03-11 14:47:56 -0500

    cambios a las variables de entorno de google_drive.py para el manejo en  loclal y luego del despliegue

--- backend/API/google/google_drive.py
+++ backend/API/google/google_drive.py
@@ -10,19 +10,24 @@ CREDENTIALS_FILE = os.path.join(BASE_DIR, 'client_secret.json')  # Ruta al JSON
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']  # Permiso para subir archivos
 
+private_key = (
+    os.getenv('PRIVATE_KEY') or  # Intenta con os.getenv (local)
+    os.environ.get('PRIVATE_KEY', '')  # Intenta con os.environ.get (producción)
+).replace('\\n', '\n')
+
 def get_drive_service():
     """Autenticación con Google Drive API usando credenciales desde variables de entorno"""
     credentials_info = {
         "type": "service_account",
-        "project_id": os.getenv('PROJECT_ID'),
-        "private_key_id": os.getenv('PRIVATE_KEY_ID'),  # Si tienes este valor
-        "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),  # Formatea correctamente el private key
-        "client_email": os.getenv('CLIENT_EMAIL'),
-        "client_id": os.getenv('CLIENT_ID'),
-        "auth_uri": os.getenv('AUTH_URI'),
-        "token_uri": os.getenv('TOKEN_URI'),
-        "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_CERT_URL'),
-        "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),  # Si tienes este valor
+        "project_id": os.environ.get('PROJECT_ID', default=os.getenv("PROJECT_ID")),
+        "private_key_id": os.environ.get('PRIVATE_KEY_ID', default=os.getenv("PRIVATE_KEY_ID")),  # Si tienes este valor
+        "private_key": private_key,  # Formatea correctamente el private key
+        "client_email": os.environ.get('CLIENT_EMAIL', default=os.getenv("CLIENT_EMAIL")),
+        "client_id":os.environ.get('CLIENT_ID', default=os.getenv("CLIENT_ID")),
+        "auth_uri": os.environ.get('AUTH_URI', default=os.getenv("AUTH_URI")),
+        "token_uri":os.environ.get('TOKEN_URI', default=os.getenv("TOKEN_URI")),
+        "auth_provider_x509_cert_url": os.environ.get('AUTH_PROVIDER_CERT_URL', default=os.getenv("AUTH_PROVIDER_CERT_URL")),
+        "client_x509_cert_url": os.environ.get('CLIENT_X509_CERT_URL', default=os.getenv("CLIENT_X509_CERT_URL")),  # Si tienes este valor
         "universe_domain": "googleapis.com"
     }
 
