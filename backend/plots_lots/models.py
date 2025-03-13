0e1224436fca3c5a1401bf74f6496326d8b2c456
Ishihara19 <stevenchicue@hotmail.com>
2025-03-13 18:54:18 -0500

    Creacion de modelo perdios, y poder registrarlo

--- backend/plots_lots/models.py
+++ backend/plots_lots/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
-# Create your models here.
+from users.models import CustomUser
+from django.core.exceptions import ValidationError
+class Plot(models.Model):
+    id_plot = models.AutoField(primary_key=True, verbose_name="ID de predio")
+    owner = models.ForeignKey(CustomUser,on_delete=models.CASCADE, related_name="Dueño_depredio")
+    plot_name = models.CharField(max_length= 20, db_index=True, null=False, blank=False, verbose_name="Nombre de predio")
+    latitud =  models.DecimalField(max_digits=9, decimal_places=6, null=False, blank=False, verbose_name="Longitud de predio")
+    longitud = models.DecimalField(max_digits=9, decimal_places=6, null=False, blank=False, verbose_name="Latitud de predio")
+    plot_extension =models.DecimalField(max_digits=4, decimal_places=2, null=False, blank=False, verbose_name="Extensión de tierra")
+    registration_date = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de registro")
+    
+    REQUIRED_FIELDS = ['plot_name', ' latitud','longitud','plot_extension']
+    
+    class Meta:
+        verbose_name = "Predio"
+        verbose_name_plural = "Predios"
+        
+    def __str__(self):
+        return f"{self.plot_name} (ID: {self.id_plot})"
+        
