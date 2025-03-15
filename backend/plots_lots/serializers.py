2c0e49f836729fa9dd2a58bbee0f88ad3ad5b54a
Ishihara19 <stevenchicue@hotmail.com>
2025-03-15 18:37:35 -0500

    Actualizacion de validacion de dueño de predio

--- backend/plots_lots/serializers.py
+++ backend/plots_lots/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
 from .models import Plot
+from users.models import CustomUser
 
 class PlotSerializer(serializers.ModelSerializer):
+    owner = serializers.CharField()
     class Meta:
         model = Plot
         fields = ['id_plot','owner','plot_name', 'latitud','longitud','plot_extension', 'registration_date']
         read_only_fields = ['id_plot', 'registration_date']  # Estos campos no se pueden modificar
+        
+
+
+    def validate_owner(self, value):
+        """Validar que el usuario existe en la base de datos."""
+        try:
+            user = CustomUser.objects.get(document=value)
+        except CustomUser.DoesNotExist:
+            raise serializers.ValidationError("El usuario no está registrado.")
+
+        return user  # Retornar el objeto en lugar del ID
 
     def validate(self, data):
-        # Validación personalizada para evitar duplicados en la georeferenciación
+        """Validación para evitar duplicados en la georeferenciación."""
         latitud = data.get('latitud')
         longitud = data.get('longitud')
         if Plot.objects.filter(latitud=latitud, longitud=longitud).exists():
