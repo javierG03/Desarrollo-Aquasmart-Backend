91bff0371d57f75e292bcdc6ded105ad36126c38
Johan Mosquerae <kira1974.dev@gmail.com>
2025-03-16 13:26:43 -0500

    Cambio de id del predio en la ruta, de int a string.

--- backend/plots_lots/urls.py
+++ backend/plots_lots/urls.py
@@ -3,5 +3,5 @@ from .views import PlotViewSet
 urlpatterns = [
     path('register', PlotViewSet.as_view({'post': 'create'}),name='registrar-predio' ),
     # Actualizar un predio 
-    path('<int:pk>/update/', PlotViewSet.as_view({'put': 'update', 'patch': 'partial_update'}), name='actualizar-predio'),
+    path('<str:pk>/update/', PlotViewSet.as_view({'put': 'update', 'patch': 'partial_update'}), name='actualizar-predio'),
 ]
