78af1abf00b8dc28e784f203bc90e5933808d31e
Johan Mosquerae <kira1974.dev@gmail.com>
2025-03-16 13:10:50 -0500

    Agregación de endpoint para actualizar predios

--- backend/plots_lots/urls.py
+++ backend/plots_lots/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .views import PlotViewSet
 urlpatterns = [
     path('register', PlotViewSet.as_view({'post': 'create'}),name='registrar-predio' ),
+    # Actualizar un predio 
+    path('<int:pk>/update/', PlotViewSet.as_view({'put': 'update', 'patch': 'partial_update'}), name='actualizar-predio'),
 ]
