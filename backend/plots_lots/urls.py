d69253bbfc37b3a0d65a1fc27557ddef9f7b6d3c
Johan Mosquerae <kira1974.dev@gmail.com>
2025-03-16 14:03:37 -0500

    Se agrega un post para poder inhabilitar o habilitar un predio, y así cambiar su estado.

--- backend/plots_lots/urls.py
+++ backend/plots_lots/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('register', PlotViewSet.as_view({'post': 'create'}),name='registrar-predio' ),
     # Actualizar un predio 
     path('<str:pk>/update/', PlotViewSet.as_view({'put': 'update', 'patch': 'partial_update'}), name='actualizar-predio'),
+    path('<str:pk>/inhabilitar/', PlotViewSet.as_view({'post': 'inhabilitar'}), name='inhabilitar-predio'),
+    path('<str:pk>/habilitar/', PlotViewSet.as_view({'post': 'habilitar'}), name='habilitar-predio'),
 ]
