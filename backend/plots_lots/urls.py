a818babc91f902f73b3d5b545f53597f09926e62
Sanviv <santiagoviveros18@gmail.com>
2025-03-18 21:49:13 -0500

    Organización de las URLs, separándolas por predio y lote

--- backend/plots_lots/urls.py
+++ backend/plots_lots/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from .views import PlotViewSet,LotCreateView,ActivateLotView, DeactivateLotView,PlotDetailView
 urlpatterns = [
+    # Rutas para predios
     path('plots/register', PlotViewSet.as_view({'post': 'create'}),name='registrar-predio' ),
     path('plots/list', PlotViewSet.as_view({'get': 'list'}), name='listar-predios'),
-    # Actualizar un predio 
     path('plots/<str:pk>/update', PlotViewSet.as_view({'put': 'update', 'patch': 'partial_update'}), name='actualizar-predio'),
     path('plots/<str:pk>/inhabilitar', PlotViewSet.as_view({'post': 'inactive'}), name='inhabilitar-predio'),
     path('plots/<str:pk>/habilitar', PlotViewSet.as_view({'post': 'active'}), name='habilitar-predio'),
+    path('plot/<str:id_plot>/', PlotDetailView.as_view(), name='plot-detail'),
+    # Rutas para lotes
     path('lots/register', LotCreateView.as_view(), name='lot-create'),
     path('lots/<str:id_lot>/activate', ActivateLotView.as_view(), name='activate-lot'),
-    path('lots/<str:id_lot>/desactivate', DeactivateLotView.as_view(), name='deactivate-lot'),    
-    path('plot/<str:id_plot>/', PlotDetailView.as_view(), name='plot-detail')
+    path('lots/<str:id_lot>/desactivate', DeactivateLotView.as_view(), name='deactivate-lot')    
 ]
