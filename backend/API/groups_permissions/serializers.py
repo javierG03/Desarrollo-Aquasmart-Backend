ca2f02b75033f642646e8514d41d96c364d1b183
Ishihara19 <stevenchicue@hotmail.com>
2025-03-21 19:16:35 -0500

    Cambios en el serializar  para mostar el grupo(rol) que pertenece

--- backend/API/groups_permissions/serializers.py
+++ backend/API/groups_permissions/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import Group, Permission
-
-
+from django.contrib.contenttypes.models import ContentType
 
 class PermissionSerializer(serializers.ModelSerializer):
     app_label = serializers.CharField(source='content_type.app_label', read_only=True)
@@ -18,16 +17,25 @@ class GroupSerializer(serializers.ModelSerializer):
         model = Group
         fields = ['id', 'name', 'permissions']
 
+class ContentTypeSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()  # Campo personalizado para el nombre legible
+
+    class Meta:
+        model = ContentType
+        fields = ['id', 'app_label', 'model', 'name']
+
+    def get_name(self, obj):
+        # Formato: "app_label | model"
+        return f"{obj.app_label} | {obj.model}" 
+        
 class GroupPermissionSerializer(serializers.ModelSerializer):
-    group_name = serializers.SerializerMethodField()
+    group_names = serializers.SerializerMethodField()
+    content_type = serializers.CharField(source='content_type.name')  # Usar el campo personalizado
 
     class Meta:
         model = Permission
-        fields = ['id', 'codename', 'name', 'content_type', 'group_name']
+        fields = ['id', 'codename', 'name', 'content_type', 'group_names']
 
-    def get_group_name(self, obj):
-        # Obtener el nombre del grupo al que pertenece el permiso
-        groups = obj.group_set.all()
-        if groups:
-            return groups[0].name  # Suponemos que un permiso pertenece a un solo grupo
-        return None        
+    def get_group_names(self, obj):
+        # Obtener los nombres de todos los grupos a los que pertenece el permiso
+        return list(obj.group_set.values_list('name', flat=True))
