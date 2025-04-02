84141d519416c6fd1daa15acb342f23490e8db68
Ishihara19 <stevenchicue@hotmail.com>
2025-04-02 11:26:23 -0500

    Solucuion de preblemas  y actualizacion del requirements.txt

--- backend/caudal/views.py
+++ backend/caudal/views.py
@@ -2,9 +2,10 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-from .models import FlowMeasurement, FlowMeasurementPredio, FlowMeasurementLote,FlowInconsistency
+from .models import FlowMeasurement, FlowMeasurementPredio, FlowMeasurementLote,FlowInconsistency, Lot,Plot
 from .serializers import FlowMeasurementSerializer,FlowMeasurementLoteSerializer, FlowMeasurementPredioSerializer,FlowInconsistencySerializer
-
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 class FlowMeasurementViewSet(viewsets.ModelViewSet):
     """
@@ -48,6 +49,12 @@ class MedicionesPredioView(APIView):
     permission_classes =[IsAuthenticated]
     
     def get(self, request, predio_id):
+        predio = get_object_or_404(Plot,plot_id=predio_id)
+
+        # Verificar que el usuario autenticado es el dueño del predio
+        if predio.owner != request.user:
+            return Response({"detail": "No tienes permiso para ver estas mediciones."}, status=status.HTTP_403_FORBIDDEN)
+    def get(self, request, predio_id):
         mediciones = FlowMeasurementPredio.objects.filter(plot_id=predio_id).order_by('-timestamp')
         serializer = FlowMeasurementPredioSerializer(mediciones, many=True)
         return Response(serializer.data)
@@ -56,6 +63,13 @@ class MedicionesLoteView(APIView):
     """Lista todas las mediciones de caudal de un lote específico"""
     
     permission_classes =[IsAuthenticated]
+    # Verificar que el usuario autenticado es el dueño del predio
+    def get(self, request, lote_id):
+        lote = get_object_or_404(Lot, lot_id=lote_id)
+        plot = lote.plot
+        # Verificar que el usuario autenticado es el dueño del lote
+        if plot.owner != request.user:
+            return Response({"detail": "No tienes permiso para ver estas mediciones."}, status=status.HTTP_403_FORBIDDEN)
     def get(self, request, lote_id):
         mediciones = FlowMeasurementLote.objects.filter(lot_id=lote_id).order_by('-timestamp')
         serializer = FlowMeasurementLoteSerializer(mediciones, many=True)
