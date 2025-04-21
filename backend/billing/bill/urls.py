cf32eddbfbbd78645e6c83a1408616482ed41eb0
iamsanviv <142766391+iamsanviv@users.noreply.github.com>
2025-04-21 14:17:30 -0500

    hotfix(endpoints-billing): Corregir implementación de endpoints (#49)

--- backend/billing/bill/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user/bills', views.UserBillListView.as_view(), name='user-bills'),  # Facturas de un usuario
-    path('admin/bills', views.AdminBillListView.as_view(), name='admin-bills'),  # Todas las facturas para admin
-]
