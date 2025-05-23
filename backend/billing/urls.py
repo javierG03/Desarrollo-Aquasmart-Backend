48f325e499613ab2f724f8483ee0cb6ac6a9df13
Steven Alexander <108034147+ishihara19@users.noreply.github.com>
2025-05-22 23:57:35 -0600

    Fix: Union mian/develop (#106)

--- backend/billing/urls.py
+++ backend/billing/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import RatesAndCompanyView
-from .bill.views import BillListView, BillDetailView,generate_monthly_bills
+from .bill.views import BillListView, BillDetailView,generate_monthly_bills, UpdateBillStatusAPIView
+
 
 urlpatterns = [
     path('rates-company', RatesAndCompanyView.as_view(), name='rates-company'), # Listar y actualizar tarifas y empresa
     path('bills', BillListView.as_view(), name='bills'),  # Listar facturas 
     path('bills/<int:pk>', BillDetailView.as_view(), name='bill-detail'),  # Ver detalle de factura 
-    path('generate-bills', generate_monthly_bills, name='generate_monthly_bills'),
+    path('generate-bills', generate_monthly_bills, name='generate_monthly_bills'),    
+    path('bills/update-status', UpdateBillStatusAPIView.as_view(), name='update-bill-status'),
+     
 
 ]
