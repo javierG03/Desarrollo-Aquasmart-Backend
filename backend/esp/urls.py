2394b9508268387fe9d24ce250f2b08ebd118947
Johan Mosquerae <kira1974.dev@gmail.com>
2025-05-26 14:19:56 -0500

    Cambios semifinales esp32

--- backend/esp/urls.py
+++ backend/esp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import recibir_consumo, enviar_comando
+from .views import recibir_consumo
 
 urlpatterns = [
     path('recibir-consumo', recibir_consumo, name='recibir-consumo'),
-    path('enviar-comandos', enviar_comando, name='enviar-comando'),
+    
 ]
